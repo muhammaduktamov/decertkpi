a918d2793a58962dccea676823a286a0fb57f9c4
muhammaduktamov <116851764+muhammaduktamov@users.noreply.github.com>
2026-04-06 16:38:32 +0500

    Update bot.py

--- dessert_bot_project/bot.py
+++ dessert_bot_project/bot.py
@@ -1,6 +1,10 @@
 import os
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from flask import Flask, request
+
+TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 users = {}
 
@@ -10,6 +14,8 @@ keyboard = [
 ]
 
 reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+app = Flask(__name__)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -76,14 +82,28 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(text)
 
 
-TOKEN = os.getenv("BOT_TOKEN")
+telegram_app = ApplicationBuilder().token(TOKEN).build()
 
-app = ApplicationBuilder().token(TOKEN).build()
+telegram_app.add_handler(CommandHandler("start", start))
+telegram_app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Bot is running"
+
+
+@app.route(f"/{TOKEN}", methods=["POST"])
+def webhook():
+    update = Update.de_json(request.get_json(force=True), telegram_app.bot)
+    telegram_app.update_queue.put_nowait(update)
+    return "ok"
 
 
 if __name__ == "__main__":
-    print("Bot started...")
-    app.run_polling()
+    import requests
+
+    # Устанавливаем webhook
+    requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}/{TOKEN}")
+
+    app.run(host="0.0.0.0", port=10000)
